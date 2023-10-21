c08d160808d1c80a71ec075c96ef5b94dbf2ce62
Daniel Watts <dcwatts007>
2023-10-20 23:20:39 -0600

    made the notification function

--- Python code/Recipe.py
+++ Python code/Recipe.py
@@ -1,3 +1,5 @@
+from plyer import notification
+import datetime
 # The Food class allows the creation of food objects that hold their name and experationDate
 class Food:
     def __init__(this,name,experationDate):
@@ -8,10 +10,39 @@ class Food:
 #The Foods Class allows an array of food objects that the user has, allowing us to search this array
 #To remove items when they are used or expire, and add items when the user buys groceries. 
 #It also is the basis for searching for recipes that the user can make by using the array of 
-#Food to 
+#Food to find recipes
 class Foods:
     def __inti__(this,food):
         this.food=food
+    def addFood(this,x):
+        {
+            this.food.append(x)
+        }
+    def removeFood(this,x):
+        this.food.remove(x)
+    #Finds the Recipes that the user can make with the food they have.
+    def find_recipes(this,recipes):
+        ingredients=[]
+        for x in this.food:
+            ingredients.append(x.name)
+        matching_recipes = []
+        for recipe, recipe_ingredients in recipes.items():
+            if all(ingredient in ingredients for ingredient in recipe_ingredients):
+                matching_recipes.append(recipe)
+        return matching_recipes
+#sends notifications about food that will expire soon. 
+    def message(this):
+        expiring=[]
+        for x in this.Food:
+            if x.experationDate.timestamp-604800<datetime.date.today().timestamp():
+                expiring.append(x)
+        if expiring.__len__!=0:
+            message=''
+            for food in expiring:
+                message += food.name+' will expire in '+(food.experationDate.timestamp()-datetime.date.today().timestapm())/86400+'days\n'
+            title = 'Food is about to expire',
+            notification.notify(title=title, message=message, timeout=30)
+
 
 if __name__ == '__main__': assert False, 'This is a class file.'
 
--- Python code/import win10toast.py
+++ Python code/import win10toast.py
@@ -1,2 +1,18 @@
-import win10toast
-print(win10toast.__file__)
+# Sample recipe database (you can expand this with more recipes and ingredients)
+
+# Function to find recipes based on available ingredients
+
+
+# Main program
+if __name__ == "__main__":
+    user_input = input("Enter a list of food items separated by commas: ").split(",")
+    user_ingredients = [item.strip().lower() for item in user_input]
+
+    matching_recipes = find_recipes(user_ingredients)
+
+    if matching_recipes:
+        print("You can make the following recipes:")
+        for recipe in matching_recipes:
+            print(recipe)
+    else:
+        print("Sorry, you don't have enough ingredients for any recipes.")
