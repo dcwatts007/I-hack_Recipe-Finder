3f85e92021abf140146561ef7900fc11ad513e60
brodricyoung <you22030@byui.edu>
2023-10-20 21:40:35 -0600

    this is my message

--- Python code/Runner.py
+++ Python code/Runner.py
@@ -6,3 +6,9 @@ from openrecipes import scrapy_prj
 toast = ToastNotifier()
 # Show the notification
 toast.show_toast("Notification", "Notification body", duration=20, icon_path="icon.ico", threaded=True)
+
+ingredient_input = input("Enter your ingredients in a list seperated by a ','. ")
+ingredient_input_list = ingredient_input.split(",")
+for ingredient in ingredient_input_list:
+    print(ingredient)
+
